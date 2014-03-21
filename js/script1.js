c658842553de2397649afb8c4cabb584ee4cdc76
Harinder Singh <harinder.singh@mastersoftwaresolutions.com>
2014-03-21 15:47:24 +0530

    Site updated: 2014-03-21 15:47:24

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,4 @@
-$("#header").click(function(){$(this).animate({left:"500px"},function(){$(this).animate({left:"0px"},1e3)})});
+$("#header").click(function(){
+    $(this).animate({left:"500px"},function(){
+    $(this).animate({left:"0px"},1e3)})
+});
